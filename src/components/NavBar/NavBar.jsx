9e72ec01a47f3c279e78988d371f5c0b21f7d52d
Ilyes Soumar Djouma <ilyes.soumar@free.fr>
2024-09-17 16:08:10 +0200

    multiple rework icon and banner

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import seeds from "/image_products/icons/color_seeds.png";
+import soils from "/image_products/icons/color_soils.png";
+import advices from "/image_products/icons/color_fields.png";
+import tools from "/image_products/icons/color_tools.png";
+import pots from "/image_products/icons/color_pot.png";
+import gallery from "/image_products/icons/color_gallery1.png";
+import plants from "/image_products/icons/color_plants1.png";
 function NavBar() {
   const [clickToAdvices, setClickToAdvices] = useState(false);
   const [clickToPhotos, setClickToPhotos] = useState(false);
@@ -19,17 +25,37 @@ function NavBar() {
 
   return (
     <nav className="nav-container">
-      <div className="nav-button">Seeds</div>
-      <div className="nav-button">Plants</div>
-      <div className="nav-button">Soil</div>
-      <div className="nav-button">Tools</div>
-      <div className="nav-button">Pots</div>
-      <div className="nav-button" onClick={handleClickToAdvices}>
-        Advices
+      <div className="nav-button">
+        <img src={seeds} alt="Seeds icon" className="img-icon" />
+        <div>Seeds</div>
       </div>
-      <div className="nav-button" onClick={handleClickToPhotos}>Photos</div>
+      <div className="nav-button">
+        <img src={soils} alt="Seeds icon" className="img-icon" />
+        <div>Soils</div>
+      </div>
+      <div className="nav-button">
+        <img src={plants} alt="Seeds icon" className="img-icon" />
+        <div>Plants</div>
+      </div>
+      <div className="nav-button">
+        <img src={tools} alt="Seeds icon" className="img-icon" />
+        <div>Tools</div>
+      </div>
+      <div className="nav-button">
+        <img src={pots} alt="Seeds icon" className="img-icon" />
+        <div>Pots</div>
+      </div>
+      <div className="nav-button" onClick={handleClickToAdvices}>
+        <img src={advices} alt="Seeds icon" className="img-icon" />
+        <div>Advices</div>
+      </div>
+      <div className="nav-button" onClick={handleClickToPhotos}>
+        <img src={gallery} alt="Seeds icon" className="img-icon" />
+        <div>Gallery</div>
+      </div>
     </nav>
   );
 }
 
 export default NavBar;
+advices;
--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -2,7 +2,16 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Logo from "../../assets/HeroSeedLogo2.png";
+import Logo1 from "/public/image_products/icons/heroseed.png";
+import cart from "/image_products/icons/color_cart.png";
+import store from "/image_products/icons/color_shop.png";
+import login from "/image_products/icons/color_login1.png";
+import logout from "/image_products/icons/color_logout1.png";
+import account from "/image_products/icons/color_account.png";
+import dark from "/image_products/icons/color_on.png";
+import light from "/image_products/icons/color_off.png";
+import moon from "/image_products/icons/color_mode_dark.png";
+import sun from "/image_products/icons/color_mode_light.png";
 import { clearToken } from "../../components/features/auth/authSlice";
 
 function Header() {
@@ -12,6 +21,7 @@ function Header() {
   const [clickToHomepage, setClickToHomepage] = useState(false);
   const [clickToAccount, setClickToAccount] = useState(false);
   const [clickToLogOut, setClickToLogOut] = useState(false);
+  const [toogleDarkMode, setToogleDarkMode] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -32,52 +42,72 @@ function Header() {
 
   const handleClickToLogOut = () => {
     setClickToLogOut(true);
-    dispatch(clearToken())
-  }
+    dispatch(clearToken());
+  };
+
+  const handleToogle = () => {
+    setToogleDarkMode(!toogleDarkMode);
+    console.log("heya");
+  };
 
   return (
-    <div className="header-container">
-      <div className="logo icon" onClick={handleClickReturnToHomepage}>
-        <img src={Logo} alt="" />
+    <div
+      className={`header-container ${
+        toogleDarkMode ? "background-light" : "background-dark"
+      }`}
+    >
+      <div className="logo-icon" onClick={handleClickReturnToHomepage}>
+        <h1>HERO</h1>
+        <img src={Logo1} alt="Logo HeroSeed" className="img-logo" />
+        <h1>SEED</h1>
       </div>
-      <div className="searchbar-container">
+      <div className= {`searchbar-container ${
+        toogleDarkMode ? "border-light " : "border-dark"
+      }`} >
         <input
           type="text"
           className="input-searchbar"
           id="input"
-          value="Rechercher un produit..."
+          defaultValue="Rechercher un produit..."
         />
         <div className="icon-search icon">
           <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" size="lg" />
         </div>
       </div>
-      <div className="mode icon">
-        <FontAwesomeIcon icon="fa-solid fa-moon" size="xl" />
-        Dark
+      <div className="mode" onClick={handleToogle}>
+        {!toogleDarkMode ? (
+          <div>
+            <img src={moon} alt="" className="icon" />
+            <img src={dark} alt="" className="icon" />
+          </div>
+        ) : (
+          <div>
+            <img src={sun} alt="" className="icon" />
+            <img src={light} alt="" className="icon" />
+          </div>
+        )}
       </div>
-      <div className="position icon" onClick={handleClickToHomepage}>
-        <FontAwesomeIcon icon="fa-solid fa-location-dot" size="xl" />
+      <div className="position" onClick={handleClickToHomepage}>
+        <img src={store} alt="" className="icon" />
         Find a store
       </div>
-      { isLogged ? (
+      {isLogged ? (
         <>
-        <div className="account icon" onClick={handleClickToAccount}>
-          <FontAwesomeIcon icon="fa-solid fa-user" size="xl" />
-          <span>My Account</span>
-        </div>
-        <div className="account icon" onClick={handleClickToLogOut}>
-        <FontAwesomeIcon icon="fa-solid fa-arrow-right-from-bracket" size="xl" />
-          <span>Log Out</span>
-        </div>
+          <div className="account" onClick={handleClickToAccount}>
+            <img src={account} alt="" className="icon" />
+            <span>My Account</span>
+          </div>
+          <div className="account" onClick={handleClickToLogOut}>
+            <img src={logout} alt="" className="icon" />
+          </div>
         </>
       ) : (
-        <div className="account icon" onClick={handleClickToAccount}>
-          <FontAwesomeIcon icon="fa-solid fa-right-to-bracket" size="xl" />
-          <span>Log In</span>
+        <div className="account" onClick={handleClickToAccount}>
+          <img src={login} alt="" className="icon" />
         </div>
       )}
-      <div className="cart icon">
-        <FontAwesomeIcon icon="fa-solid fa-cart-shopping" size="xl" />
+      <div className="cart">
+        <img src={cart} alt="" className="icon" />
         My Cart
       </div>
     </div>
